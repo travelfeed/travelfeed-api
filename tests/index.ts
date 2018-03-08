c66701b0dd38ad2cc9f6c304f1cd847dd6c29b2b
Pascal Iske <info@pascal-iske.de>
2018-03-08 15:08:34 +0100

    chore(tests): fix test cases

--- tests/index.ts
+++ tests/index.ts
@@ -3,11 +3,29 @@ import * as supertest from 'supertest'
 import { server } from '../src/server'
 
 test('GET /', async t => {
-    t.plan(3)
+    t.plan(1)
 
     const res = await supertest(server).get('/')
 
-    t.is(res.status, 500)
-    t.is(res.body.status, 500)
-    t.is(res.body.message, 'Internal server error')
+    t.is(res.status, 404)
+})
+
+test('GET /api/auth', async t => {
+    t.plan(3)
+
+    const res = await supertest(server).get('/api/auth')
+
+    t.is(res.status, 200)
+    t.is(res.body.status, 200)
+    t.is(res.body.data, 'Hello user!')
+})
+
+test('GET /api/auth/signin', async t => {
+    t.plan(3)
+
+    const res = await supertest(server).get('/api/auth/signin')
+
+    t.is(res.status, 200)
+    t.is(res.body.status, 200)
+    t.is(res.body.data, 'Signin!')
 })
