b7079bed40a8d77318f6d22d2bf07ce5f33dab15
Pascal Iske <info@pascal-iske.de>
2018-03-14 15:21:54 +0100

    fix(jwt.strategy): remove console log

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -5,8 +5,6 @@ import { User } from '../../user/models/user.model'
 
 export const JwtStrategy = new Strategy(jwtConfig, async (payload, next) => {
     try {
-        console.log(`Payload received: ${JSON.stringify(payload)}`)
-
         const repository: Repository<User> = getManager().getRepository(User)
         const user: User = await repository.findOneById(payload.userId)
 
