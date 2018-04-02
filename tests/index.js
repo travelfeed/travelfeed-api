2952f4f57795fbec229fab17753660a1d1261c06
Pascal Iske <info@pascal-iske.de>
2018-04-02 14:18:02 +0200

    fix(tests): correct renamed function in tests

--- tests/index.js
+++ tests/index.js
@@ -1,11 +1,11 @@
 import test from 'ava'
 import { agent } from 'supertest'
-import { server } from '../dist/server'
+import { initServer } from '../dist/server'
 
 test('GET /', async t => {
     t.plan(3)
 
-    const res = await agent(await server(3000)).get('/')
+    const res = await agent(await initServer(3000)).get('/')
 
     t.is(res.status, 200)
     t.is(res.body.status, 200)
