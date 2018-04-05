c0d0a79f851923feeb8c2b2f0efc23e5b85eb044
Pascal Iske <info@pascal-iske.de>
2018-04-05 23:39:58 +0200

    fix(socket): update socket class

--- src/config/socket.ts
+++ src/config/socket.ts
@@ -1,22 +1,31 @@
-import * as socketIo from 'socket.io'
+import * as io from 'socket.io'
 import { Server } from 'http'
 import { logger, Handlers } from '../logger'
 
+export interface SocketEvent {
+    name: 'message' | 'custom'
+    data: any
+}
+
 export class Socket {
     private logger: Handlers = logger('socket')
-    private io: socketIo.Server
+    private io: io.Server
 
     public constructor(server: Server) {
-        this.io = socketIo(server)
-        this.initSocket()
-    }
+        this.io = io(server)
+        this.io.on('connect', (socket: io.Socket) => {
+            this.logger.debug(`user connected (#${socket.id})`)
 
-    private initSocket(): void {
-        this.io.on('connection', socket => {
-            this.logger.debug('a user connected')
+            socket.on('message', event => {
+                this.logger.silly('message', event)
+            })
+
+            socket.on('custom', event => {
+                this.logger.silly('custom', event)
+            })
 
             socket.on('disconnect', () => {
-                this.logger.debug('a user disconnected')
+                this.logger.debug(`user disconnected (#${socket.id})`)
             })
         })
     }
