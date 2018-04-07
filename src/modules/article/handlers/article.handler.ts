93bb1878b70608f2fea5206f0d22de7488aca54f
Lars Waechter <lawaendel@googlemail.com>
2018-04-07 16:10:35 +0200

    add create article handler

--- src/modules/article/handlers/article.handler.ts
+++ src/modules/article/handlers/article.handler.ts
@@ -1,19 +1,32 @@
 import { Request, Response, NextFunction } from 'express'
 import { getManager, Repository } from 'typeorm'
 import { bind } from 'decko'
+import { escape } from 'validator'
 import { Article } from '../models/article.model'
+import { User } from '../../user/models/user.model'
+import { ArticleText } from '../models/article.text.model'
+import { Language } from '../../misc/models/language.model'
+import { Picture } from '../../misc/models/picture.model'
 
 export class ArticleHandler {
-    private repository: Repository<Article>
+    private articleRepo: Repository<Article>
+    private userRepo: Repository<User>
+    private textRepo: Repository<ArticleText>
+    private langRepo: Repository<Language>
+    private picRepo: Repository<Picture>
 
     public constructor() {
-        this.repository = getManager().getRepository(Article)
+        this.articleRepo = getManager().getRepository(Article)
+        this.userRepo = getManager().getRepository(User)
+        this.textRepo = getManager().getRepository(ArticleText)
+        this.langRepo = getManager().getRepository(Language)
+        this.picRepo = getManager().getRepository(Picture)
     }
 
     @bind // read all articles
     public async readArticles(req: Request, res: Response, next: NextFunction): Promise<void> {
         try {
-            const data: Array<Article> = await this.repository.find({
+            const data: Array<Article> = await this.articleRepo.find({
                 relations: [
                     'user',
                     'articleText',
@@ -32,7 +45,7 @@ export class ArticleHandler {
     @bind // read ten newest articles
     public async readNewestArticles(req: Request, res: Response, next: NextFunction) {
         try {
-            const data: Array<Article> = await this.repository.find({
+            const data: Array<Article> = await this.articleRepo.find({
                 relations: [
                     'user',
                     'articleText',
@@ -54,7 +67,7 @@ export class ArticleHandler {
     @bind // read ten best rated articles
     public async readBestRatedArticles(req: Request, res: Response, next: NextFunction) {
         try {
-            const data: Array<Article> = await this.repository.find({
+            const data: Array<Article> = await this.articleRepo.find({
                 relations: [
                     'user',
                     'articleText',
@@ -79,15 +92,55 @@ export class ArticleHandler {
     public async createArticle(req: Request, res: Response, next: NextFunction): Promise<void> {
         try {
             // create new empty article instance
-            const newArticle: Article = this.repository.create()
+            const newArticle: Article = this.articleRepo.create()
 
-            // add further props here
-            newArticle.country = req.body.country
+            // article details
+            newArticle.title = escape(req.body.title || '')
+            newArticle.country = escape(req.body.country || '')
+            newArticle.city = escape(req.body.city || '')
+            newArticle.latitude = req.body.latitude
+            newArticle.longitude = req.body.longitude
+            newArticle.user = await this.userRepo.findOneById(req.user.id)
 
             // save new article
-            await this.repository.save(newArticle)
+            await this.articleRepo.save(newArticle)
 
-            res.json({ status: res.statusCode, data: newArticle })
+            // loop through article texts and store each one
+            for (const articleText of req.body.articleText) {
+                // create new empty articleText instance
+                const newArticleText: ArticleText = await this.textRepo.create()
+
+                // articleText details
+                newArticleText.article = newArticle
+                newArticleText.text = escape(articleText.text)
+                newArticleText.language = await this.langRepo.findOneById(articleText.language || 1)
+                newArticleText.article = newArticle
+
+                // save new articleText
+                await this.textRepo.save(newArticleText)
+            }
+
+            // loop through article pictures and store each one
+            for (const articlePic of req.body.articlePicture) {
+                // create new empty picture instance
+                const newArticlePic: Picture = await this.picRepo.create()
+
+                // picture details
+                newArticlePic.article = newArticle
+                newArticlePic.link = articlePic.link
+                newArticlePic.title = escape(articlePic.text || '')
+                newArticlePic.alt = escape(articlePic.alt || '')
+
+                // save new picture
+                await this.picRepo.save(newArticlePic)
+            }
+
+            // load new article data
+            const data = await this.articleRepo.findOneById(newArticle.id, {
+                relations: ['user', 'articleText', 'articleText.language', 'pictures']
+            })
+
+            res.json({ status: res.statusCode, data: data })
         } catch (err) {
             next(err)
         }
@@ -96,7 +149,7 @@ export class ArticleHandler {
     @bind
     public async readArticle(req: Request, res: Response, next: NextFunction): Promise<void> {
         try {
-            const data: Article = await this.repository.findOneById(req.params.articleId, {
+            const data: Article = await this.articleRepo.findOneById(req.params.articleId, {
                 relations: [
                     'user',
                     'articleText',
@@ -116,7 +169,7 @@ export class ArticleHandler {
     @bind
     public async updateArticle(req: Request, res: Response, next: NextFunction): Promise<void> {
         try {
-            const article: Article = await this.repository.findOneById(req.params.articleId, {
+            const article: Article = await this.articleRepo.findOneById(req.params.articleId, {
                 relations: ['user', 'articleText', 'articleText.language', 'pictures']
             })
 
@@ -124,7 +177,7 @@ export class ArticleHandler {
             article.title = req.body.title
 
             // save updated article
-            const updatedArticle = await this.repository.save(article)
+            const updatedArticle = await this.articleRepo.save(article)
 
             res.json({ status: res.statusCode, data: updatedArticle })
         } catch (err) {
@@ -135,13 +188,13 @@ export class ArticleHandler {
     @bind
     public async deleteArticle(req: Request, res: Response, next: NextFunction): Promise<void> {
         try {
-            const article: Article = await this.repository.findOneById(req.params.articleId)
+            const article: Article = await this.articleRepo.findOneById(req.params.articleId)
 
             // delete article
-            await this.repository.delete(article)
+            await this.articleRepo.delete(article)
 
             // search for deleted article
-            const articleExists = await this.repository.findOneById(req.params.articleId)
+            const articleExists = await this.articleRepo.findOneById(req.params.articleId)
 
             if (!articleExists) {
                 res.json({ status: res.statusCode, data: 'article was deleted' })
